ddd1eff811207cd16a8090bae1d850b10c9e58c6
sunghye-on <opei71@naver.com>
2020-04-08 17:22:39 +0900

    shallowEqual을 이용한 비효율적인 리렌더링 최적화

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Counter from "../components/Counter";
-//상태를 조회하기위한 useSelector   dispatch를 위함
-import { useSelector, useDispatch } from "react-redux";
+//상태를 조회하기위한 useSelector   dispatch를 위함   효율적인 리렌더링을 위한 shallowEqual
+import { useSelector, useDispatch, shallowEqual } from "react-redux";
 //액션 생성함수를 불러와서 디스패치함
 import { increase, decrease, setDiff } from "../modules/counter";
 
 function CounterContainer() {
   //useSelector를이용해서 해당 state의 값들을 객체향태로 얻어 낸뒤 비구조 할당으로 값을 바로 조회가능하도록만듬
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    (state) => ({
+      //useSelect를 사용해서 비효율적으로 리렌더링되고 있음이 감지 되었다 이럴때는 최척화를 위해 useSelect의 파라미터 equalityFn을 지정해준다.
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    //모든 요소들을 검사해서 맞는지 확인을 위해  shallowEqual을 추가해줌으로 아래의 코드를 대신한다
+    // (left, right) => {
+    //   return right.diff === left.diff && right.number === left.number;
+    // }
+    shallowEqual
+  );
   const dispatch = useDispatch();
 
   //미리 만들어 놓은 액션생성 함수를 이용하여 디스패치
